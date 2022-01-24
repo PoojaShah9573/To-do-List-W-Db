11ff05ce2e06a4a5224210cfff1707d965340d66
Pooja Shah <pshah_be19@thapar.edu>
2022-01-24 23:00:27 +0545

    Add gitignore,procfile and update

--- app.js
+++ app.js
@@ -155,7 +155,13 @@
    });
  });
 
- app.listen(3000, function ()
+ let port = process.env.PORT;
+ if (port == null || port == "") {
+   port = 3000;
+ }
+
+
+ app.listen(port, function ()
  {
-    console.log("Server started on port 3000");
+    console.log("Server started succesfully");
  });
